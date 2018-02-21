61a451821873004b1ca7e0742920da6126a34d60
Sartaglo <dolha@ualberta.ca>
2018-02-21 15:52:34 -0700

    Support arbitrary Task titles

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Task.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Task.java
@@ -2,12 +2,15 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 final class Task
 {
+    final private String title;
+
     Task(String title, String description)
     {
+        this.title = title;
     }
 
     String getTitle()
     {
-        return "title";
+        return this.title;
     }
 }
