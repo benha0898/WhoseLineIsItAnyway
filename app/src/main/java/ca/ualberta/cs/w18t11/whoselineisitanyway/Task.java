fba4ce4e1157ef5521784c3a6c1ac7bfb8c77ab0
Sartaglo <dolha@ualberta.ca>
2018-02-21 15:50:07 -0700

    Implement Task.getTitle

--- app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Task.java
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Task.java
@@ -2,4 +2,12 @@ package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 final class Task
 {
+    Task(String title, String description)
+    {
+    }
+
+    String getTitle()
+    {
+        return "title";
+    }
 }
