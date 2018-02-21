14589f7ad794e52796eacbbadb82602410c089b5
Sartaglo <dolha@ualberta.ca>
2018-02-21 15:47:20 -0700

    Add Task getTitle test

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/TaskUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/TaskUnitTest.java
@@ -1,5 +1,15 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 final public class TaskUnitTest
 {
+    @Test
+    final public void testGetTitle()
+    {
+        final String title = "title";
+        final Task task = new Task(title, "");
+        Assert.assertEquals(title, task.getTitle());
+    }
 }
