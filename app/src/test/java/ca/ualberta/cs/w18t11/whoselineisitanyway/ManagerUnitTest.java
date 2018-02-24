24f384c25bd95d2eb3c5ba1a8569eebcd071d0e5
Mark Griffith <mgriffit@ualberta.ca>
2018-02-24 13:15:47 -0700

    Add Manager getter tests

--- app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/ManagerUnitTest.java
+++ app/src/test/java/ca/ualberta/cs/w18t11/whoselineisitanyway/ManagerUnitTest.java
@@ -1,26 +1,34 @@
 package ca.ualberta.cs.w18t11.whoselineisitanyway;
 
 import junit.framework.Assert;
-
 import org.junit.Test;
+import java.math.BigDecimal;
 
 public class ManagerUnitTest
 {
     @Test(expected = IndexOutOfBoundsException.class)
-    final public void testGetOutOfRange()
-    {
+    final public void testGetOutOfRange() {
         Manager<Integer> manager = new Manager<Integer>();
         manager.get(1);
     }
 
     @Test
-    final public void testGetTask()
-    {
+    final public void testGetTask() {
         Manager<Task> taskManager = new Manager<Task>();
-        Task task = new Task("name","description");
-        Task wrongTask = new Task("name","description");
-        taskManager.add(task);
-        Assert.assertEquals(task, taskManager.get(0));
+        Task addedTask = new Task("name", "description");
+        Task wrongTask = new Task("name", "description");
+        taskManager.add(addedTask);
+        Assert.assertEquals(addedTask, taskManager.get(0));
         Assert.assertNotSame(wrongTask, taskManager.get(0));
+    }
+
+    @Test
+    final public void testGetBid() {
+        Manager<Bid> taskManager = new Manager<Bid>();
+        Bid addedBid = new Bid("provider", "task", new BigDecimal(1));
+        Bid wrongBid = new Bid("provider", "task", new BigDecimal(1));
+        taskManager.add(addedBid);
+        Assert.assertEquals(addedBid, taskManager.get(0));
+        Assert.assertNotSame(wrongBid, taskManager.get(0));
     }
 }
