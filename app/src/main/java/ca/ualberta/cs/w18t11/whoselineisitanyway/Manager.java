8c28daeefd0093ebcb124f9a4d168e5cb21e5c53
Mark Griffith <mgriffit@ualberta.ca>
2018-02-24 12:12:48 -0700

    Create Manager class

---
+++ app/src/main/java/ca/ualberta/cs/w18t11/whoselineisitanyway/Manager.java
@@ -0,0 +1,33 @@
+package ca.ualberta.cs.w18t11.whoselineisitanyway;
+
+import java.util.ArrayList;
+
+public class T Manager
+{
+    private ArrayList<T> items;
+
+    Manager()
+    {
+
+    }
+
+    public T get()
+    {
+
+    }
+
+    public void add()
+    {
+
+    }
+
+    public void set(int index, T value)
+    {
+
+    }
+
+    public void delete(int index)
+    {
+
+    }
+}
